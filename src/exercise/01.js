f489e42f7348bb54e351235d1fa4036c0b8adbf4
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-07 12:07:45 +0300

    Ex 1 extra 1 Handle promise error with custom PokemonErrorBoundary

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,19 +2,21 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {fetchPokemon, PokemonDataView} from '../pokemon'
+import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 let pokemon
-
+let error
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
-const pokemonPromise = fetchPokemon('pikachu')
-.then(data => { pokemon = data })
+const pokemonName = Math.random() > 0.5 ? 'pikachu' : 'ddd'
+const pokemonPromise = fetchPokemon(pokemonName)
+.then(data => { pokemon = data }, e => { error = e })
 
 function PokemonInfo() {
+  if (error) throw error
   if (!pokemon) throw pokemonPromise
 
   // if the code gets it this far, then the pokemon variable is defined and
@@ -33,9 +35,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={<div>loading...</div>}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<div>loading...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
